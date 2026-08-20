018fe5677e938c422ca176f40da267c049807f97
parwow <parksh112-@naver.com>
2026-08-20 11:19:02 +0900

    main.gd 업데이트

--- main.gd
+++ main.gd
@@ -1,9 +1,18 @@
 @tool
 extends EditorPlugin
 
+@tool
+extends EditorPlugin
+
+const SETTING_PATH := "auto_save/interval"
+
 
 func _enter_tree():
-	print("turn on")
+	if not ProjectSettings.has_setting(SETTING_PATH):
+		ProjectSettings.set_setting(SETTING_PATH, 60)	ProjectSettings.set_initial_value(SETTING_PATH, 60)
+	ProjectSettings.set_as_basic(SETTING_PATH, true)
+
+	print("save seconds: ", ProjectSettings.get_setting(SETTING_PATH))
 
 
 func _exit_tree():
