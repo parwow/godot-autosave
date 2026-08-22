ef4f0ba834df8d6d83208ca5ef0bfac1a0038614
parwow <parksh112-@naver.com>
2026-08-22 10:19:07 +0900

    Implement auto-save functionality with timer
    Refactor auto-save plugin to use a timer for periodic saves.

--- main.gd
+++ main.gd
@@ -1,19 +1,42 @@
 @tool
 extends EditorPlugin
 
-@tool
-extends EditorPlugin
+const AUTO_SAVE_TIME: float = 30.0
 
-const SETTING_PATH := "auto_save/interval"
-
-
-func _enter_tree():
-	if not ProjectSettings.has_setting(SETTING_PATH):
-		ProjectSettings.set_setting(SETTING_PATH, 60)	ProjectSettings.set_initial_value(SETTING_PATH, 60)
-	ProjectSettings.set_as_basic(SETTING_PATH, true)
-
-	print("save seconds: ", ProjectSettings.get_setting(SETTING_PATH))
+var timer: Timer
 
 
-func _exit_tree():
-	print("turn off")
+func _enter_tree() -> void:
+	print("[Auto Save+] 플러그인 시작")
+
+	timer = Timer.new()
+	timer.wait_time = AUTO_SAVE_TIME
+	timer.one_shot = false
+	timer.autostart = true
+	timer.timeout.connect(_auto_save)
+
+	add_child(timer)
+
+	print("[Auto Save+] 30초마다 자동 저장합니다.")
+
+
+func _exit_tree() -> void:
+	print("[Auto Save+] 플러그인 종료")
+
+	if timer:
+		timer.timeout.disconnect(_auto_save)
+		timer.stop()
+		timer.queue_free()
+		timer = null
+
+
+func _auto_save() -> void:
+	print("[Auto Save+] 자동 저장 실행")
+
+	if EditorInterface.get_edited_scene_root() == null:
+		print("[Auto Save+] 저장할 씬이 없습니다.")
+		return
+
+	EditorInterface.save_scene()
+
+	print("[Auto Save+] 저장 완료")
